d167b25b127d3603d37de0bd9b411d99eb12ad1a
aleksander.wojciechowski <aleksander.wojciechowski@capgemini.com>
2024-09-20 13:36:46 +0200

    Counter

--- app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
+++ app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
@@ -9,6 +9,7 @@ import com.awstraining.backend.api.rest.v1.model.Measurement;
 import com.awstraining.backend.api.rest.v1.model.Measurements;
 import com.awstraining.backend.business.measurements.MeasurementDO;
 import com.awstraining.backend.business.measurements.MeasurementService;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +17,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("device/v1")
 class DeviceController implements DeviceIdApi {
     private static final Logger LOGGER = LogManager.getLogger(DeviceController.class);
 
     private final MeasurementService service;
+    private final MeterRegistry meterRegistry;
 
     @Autowired
-    public DeviceController(final MeasurementService service) {
+    public DeviceController(final MeasurementService service, MeterRegistry meterRegistry) {
         this.service = service;
+        this.meterRegistry = meterRegistry;
     }
 
     @Override
     public ResponseEntity<Measurement> publishMeasurements(final String deviceId, final Measurement measurement) {
         LOGGER.info("Publishing measurement for device '{}'", deviceId);
+        meterRegistry.counter("publishMeasurements", "method", "invocation").increment();
         final MeasurementDO measurementDO = fromMeasurement(deviceId, measurement);
         service.saveMeasurement(measurementDO);
         return ResponseEntity.ok(measurement);
@@ -45,8 +50,7 @@ class DeviceController implements DeviceIdApi {
                 .toList();
         final Measurements measurementsResult = new Measurements();
         measurementsResult.measurements(measurements);
-        LOGGER.info("AWO 000000000000000 classic log");
-        LOGGER.info("LOGGING a size of retrieveMeasurements '{}'", measurements.size());
+
         return ResponseEntity.ok(measurementsResult);
     }
 
